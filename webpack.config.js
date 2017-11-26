7484311c74ce49e64b0acec16f74b95ab4d09d82
Joel Longie <admin@joellongie.com>
2017-11-26 14:02:43 -0700

    output compress and uncompressed

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const CSS_LOADER = PRODUCTION
+? ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: [
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true,
+      },
+    },
+  ],
+})
+: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'];
+
+
+module.exports = {
+  entry: {
+    bundle: "./src/js/main.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.min.js",
+  },
+  devtool: PRODUCTION ? '' : 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },]
+        }),
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: PRODUCTION ? 'main.min.css' : 'main.css',
+      allChunks: true,
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['./'] }
+    })
+  ]
+}
